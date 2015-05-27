ed39a5574bd1195881b9a0734f077bb9e14ca69b
pkufashuo400 <pkufashuo400@gmail.com>
2015-05-28 01:08:44 +0800

    Upgrade to keystone 3.0

--- keystone.js
+++ keystone.js
@@ -29,11 +29,13 @@ keystone.init({
 		extname: '.hbs'
 	}).engine,
 	
+	'emails': 'templates/emails',
+	
 	'auto update': true,
 	'session': true,
 	'auth': true,
 	'user model': 'User',
-	'cookie secret': '93CV/Uz(93G>H1TJ@5H9]E#&R1&1WF+g`u/]KTHKET:fal=daW0<OSBuLty)G/IB'
+	'cookie secret': 'H?eL9>pkdR:(4HJ%dpSp;s8"{%XFab`Uy8E?rr<px#BhF4?A_,HL@9K~FZ|lRwoN'
 
 });
 
@@ -58,6 +60,39 @@ keystone.set('routes', require('./routes'));
 
 // Setup common locals for your emails. The following are required by Keystone's
 // default email templates, you may remove them if you're using your own.
+
+keystone.set('email locals', {
+	logo_src: '/images/logo-email.gif',
+	logo_width: 194,
+	logo_height: 76,
+	theme: {
+		email_bg: '#f9f9f9',
+		link_color: '#2697de',
+		buttons: {
+			color: '#fff',
+			background_color: '#2697de',
+			border_color: '#1a7cb7'
+		}
+	}
+});
+
+// Setup replacement rules for emails, to automate the handling of differences
+// between development a production.
+
+// Be sure to update this rule to include your site's actual domain, and add
+// other rules your email templates require.
+
+keystone.set('email rules', [{
+	find: '/images/',
+	replace: (keystone.get('env') == 'production') ? 'http://www.your-server.com/images/' : 'http://localhost:3000/images/'
+}, {
+	find: '/keystone/',
+	replace: (keystone.get('env') == 'production') ? 'http://www.your-server.com/keystone/' : 'http://localhost:3000/keystone/'
+}]);
+
+// Load your project's email test routes
+
+keystone.set('email tests', require('./routes/emails'));
 
 // Configure the navigation bar in Keystone's Admin UI
 
--- models/Enquiry.js
+++ models/Enquiry.js
@@ -21,8 +21,45 @@ Enquiry.add({
 		{ value: 'other', label: 'Something else...' }
 	] },
 	message: { type: Types.Markdown, required: true },
-	createdAt: { type: Date, 'default': Date.now }
+	createdAt: { type: Date, default: Date.now }
 });
+
+Enquiry.schema.pre('save', function(next) {
+	this.wasNew = this.isNew;
+	next();
+});
+
+Enquiry.schema.post('save', function() {
+	if (this.wasNew) {
+		this.sendNotificationEmail();
+	}
+});
+
+Enquiry.schema.methods.sendNotificationEmail = function(callback) {
+	
+	if ('function' !== typeof callback) {
+		callback = function() {};
+	}
+	
+	var enquiry = this;
+	
+	keystone.list('User').model.find().where('isAdmin', true).exec(function(err, admins) {
+		
+		if (err) return callback(err);
+		
+		new keystone.Email('enquiry-notification').send({
+			to: admins,
+			from: {
+				name: 'hkite',
+				email: 'contact@hkite.com'
+			},
+			subject: 'New Enquiry for hkite',
+			enquiry: enquiry
+		}, callback);
+		
+	});
+	
+};
 
 Enquiry.defaultSort = '-createdAt';
 Enquiry.defaultColumns = 'name, email, enquiryType, createdAt';
