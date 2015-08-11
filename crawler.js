025c7bc3fd1f9e86f5fc283964df10fddf22908a
carolusian <pkufashuo400@gmail.com>
2015-08-11 22:24:06 +0800

    Change to js-crawler (PhantomJS based)

--- crawler.js
+++ crawler.js
@@ -1,15 +1,15 @@
 /**
  * Prelimary testing 
- * Use dalekjs as a crawler
+ * Use js-crawler
  * @author Charlie Chen
  */
 
- module.exports = {
- 	'Able to run on linux server': function(test) {
- 		test
- 			.open('http://weixin.sogou.com/weixin?query=hkite&type=2&page=1&ie=utf8')
- 			.assert.title().is('hkite的相关微信公众号文章 – 搜狗微信搜索', 'It has title')
- 			.done();
-
- 	}
- }
+var Crawler = require("js-crawler");
+ 
+var crawler = new Crawler().configure({
+	depth:2
+});
+ 
+crawler.crawl("http://weixin.sogou.com/weixin?query=hkite&type=2&page=1&ie=utf8", function(page) {
+  console.log(page.url);
+});
