3b30b8686436a503d3b1d8ad95b0b955307242f1
carolusian <pkufashuo400@gmail.com>
2015-08-27 10:16:07 +0800

    able to get wechat posts

--- crawler.js
+++ crawler.js
@@ -7,9 +7,33 @@
 require('dotenv').load();
 
 var _         = require('underscore'),
+	fs        = require('fs'),
     Nightmare = require('nightmare'),
     cheerio   = require('cheerio'),
-    request   = require('request');
+    request   = require('request'),
+    truncate  = require('html-truncate'),
+    slug      = require('limax'),
+    cloudinary = require('cloudinary'),
+	mongoose  = require('mongoose');
+
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+mongoose.connect(process.env.MONGO_URI);
+
+var Post = mongoose.model('Post',  {
+	title: 'String',
+	state: 'String',
+	slug: 'String',
+	publishedDate: 'Date',
+	content: {
+		brief: 'String',
+		extended: 'String'
+	}
+});
 
 /**
  * Get list of articles from the entry of 
@@ -84,20 +108,69 @@ function readPost(link) {
 }
 
 function savePost(post) {
-	var mongoose = require('mongoose');
-	mongoose.connect('mongodb://localhost/hkite');
-	var Post = mongoose.model('Post', {title: 'String'});
+	var $ = cheerio.load(post);
 
-	var post = new Post({title: 'Zebra'});
+	// Get "View Origin Post" Link
+	var re = /var msg_source_url = \'(.+)\'\;/gi;
+	var found = re.exec(post);
+	var sourceLink = "";
+	if(found != null && found.length > 1)
+		sourceLink = found[1];
+	$('#js_view_source').attr('href', sourceLink);
+
+	// Get post title
+	var postTitle = $('#activity-name').text().trim();
+	// Remove post title from post content
+	$('#activity-name').remove();
+
+	// Get post brief
+	var postBrief = truncate($('#js_content').text().trim(), 100);
+
+	// Get post content
+	var postContent = $('#page-content').html();
+	
+	// Get post date
+	var postDate = $('#post-date').text().trim();
+
+	// Upload image
+	var cover = null;
+	$ = cheerio.load($('#js_content').html());
+	var imgs = $('img');
+	if(imgs['0'] != undefined) {
+
+	} else {
+
+	}
+
+	var post = new Post({
+		title: postTitle,
+		content: {
+			brief: postBrief,
+			extended: postContent
+		},
+		publishedDate: postDate, 
+		state: 'published',
+		slug: slug(postTitle) 
+	});
 
 	post.save(function(err) {
 		if(err) console.log(err);
 		console.log('meow');
-		mongoose.connection.close();
 	});
+}
+
+function gracefulExit() {
+	mongoose.connection.close(function () {
+    	console.log('Mongoose default connection is disconnected through app termination');
+   		process.exit(0);
+  	});
 }
 
 var pagenums = 1;
 
-var url = "http://weixin.sogou.com/gzh?openid=oIWsFt-vDiyfe6oT4lc31FjbJXNs";
-crawl(url);
+var url = process.env.WEIXIN_ACCOUNT_URL;
+// crawl(url);
+var content = fs.readFileSync('http://mmbiz.qpic.cn/mmbiz/RT3nzzJnPhricfPaz43DjFonX3Rvk59Pg91uWqYgdIXWbug7pYoo7qQFJdQUNia5YHOXAOCTib2q3ZibZ89IUML1Dw/0?wx_fmt=png');
+console.log(content);
+
+process.on('SIGINT', gracefulExit).on('SIGTERM', gracefulExit);
